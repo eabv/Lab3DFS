93af40ecf027b06c3587cb32f2534adb589eb8ed
Erika <eabenalcazar2@espe.edu.ec>
2022-06-03 01:24:06 -0500

    Ḿétodo agregación de una arista

--- [NRC_6278]_ErikaBenalcazar_Lab3Unidad1.py
+++ [NRC_6278]_ErikaBenalcazar_Lab3Unidad1.py
@@ -53,3 +53,27 @@ class Grafo:
         # Para la implementación de la lista de adyacencia se usa un diccionario
         self.m_lista_adyacencia = {node: set() for node in self.m_Nodos}
 
+    # Añande una arista al grafo
+    def agregar_Arista(self, nodo1, nodo2, peso=1):
+        '''
+        Agrega una arista a los nodos del grafo, además añade el peso de cada 
+        arista.
+
+        Parámetros
+        ----------
+        nodo1 : int
+            Entrada de dato tipo entero para el nodo1 
+        nodo2 : int
+            Entrada de dato tipo entero para el nodo2 
+        peso : int
+            Entrada de dato tipo entero para el 
+        '''
+
+        #En la lista de adyacencia toma el nodo 1 y agrega el nodo 2 y su peso.
+        self.m_lista_adyacencia[nodo1].add((nodo2, peso))
+
+        # Agrega una arista a otro nodo cuando el grafo no sea dirigido
+        if not self.m_dirigido:
+            # Se agrega al nodo 2, el nodo 1 y su peso
+            self.m_lista_adyacencia[nodo2].add((nodo1, peso))
+
